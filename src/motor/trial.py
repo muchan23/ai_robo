2816815e9620159308d2231ee402ec39c6fdb1aa
muchan23 <murakami-yuki866@g.ecc.u-tokyo.ac.jp>
2025-10-21 22:36:30 +0900

    modify motor

--- src/motor/trial.py
+++ src/motor/trial.py
@@ -1,25 +1,17 @@
 from time import sleep
-import pigpio
+import RPi.GPIO as GPIO
 import time
 import sys
 
 #GPIO???
 try:
-    pi = pigpio.pi()
-    if not pi.connected:
-        print("pigpioデーモンに接続できません。")
-        print("以下のコマンドでpigpioデーモンを起動してください：")
-        print("sudo pigpiod")
-        sys.exit(1)
-    
-    pi.set_mode(17, pigpio.OUTPUT)
-    pi.set_mode(22, pigpio.OUTPUT)
-    print("pigpioデーモンに正常に接続しました。")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(22, GPIO.OUT)
+    print("GPIOが正常に初期化されました。")
     
 except Exception as e:
-    print(f"pigpioの初期化でエラーが発生しました: {e}")
-    print("pigpioデーモンが起動しているか確認してください：")
-    print("sudo pigpiod")
+    print(f"GPIOの初期化でエラーが発生しました: {e}")
     sys.exit(1)
 
 #PWM?????
@@ -29,26 +21,33 @@ freq = 100 #PWM????Hz???
 up_flag = True
 
 #IN1?IN2?????
-pi.write(17, 0)
-pi.write(22, 1)
+GPIO.output(17, GPIO.LOW)
+GPIO.output(22, GPIO.HIGH)
+
+#PWM???
+pwm = GPIO.PWM(pwm_pin1, freq)
+pwm.start(duty)
 
 #???????
-while True:
-  
-    #???????????
-    cnv_dutycycle = int((duty * 1000000 / 100))
-    #PWM???
-    pi.hardware_PWM(pwm_pin1, freq, cnv_dutycycle)
-    #duty???
-    if up_flag == True:
-        if duty == 100:
-            up_flag = False
+try:
+    while True:
+        #PWM???
+        pwm.ChangeDutyCycle(duty)
+        #duty???
+        if up_flag == True:
+            if duty == 100:
+                up_flag = False
+            else:
+                duty += 1
         else:
-            duty += 1
-    else:
-        if duty == 0:
-            up_flag = True
-        else:
-            duty -=1
-    
-    time.sleep(0.05)
+            if duty == 0:
+                up_flag = True
+            else:
+                duty -=1
+        
+        time.sleep(0.05)
+
+except KeyboardInterrupt:
+    print("\nプログラムを終了します...")
+    pwm.stop()
+    GPIO.cleanup()
